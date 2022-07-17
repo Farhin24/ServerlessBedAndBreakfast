104b35716113966c30a38a5ff65a4c9f92090f8e
Bijitashya Birinchi <bijitashyabirinchi@Bijitashyas-MacBook-Pro.local>
2022-07-16 23:47:31 -0300

    init client app

---
+++ client/src/booking/Tour.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { allTours } from "../actions/hotel";
+import Search from "../components/forms/Search";
+import TourCard from "../components/cards/TourCard";
+
+const Tour = () => {
+  const [tour, setTour] = useState([]);
+
+  useEffect(() => {
+    loadAllTours();
+  }, []);
+
+  const loadAllTours = async () => {
+    let res = await allTours();
+    setTour(res.data.body);
+  };
+
+  return (
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h1>Tour Packages</h1>
+      </div>
+      {/* <div className="col">
+        <br />
+        <Search />
+      </div> */}
+      <div className="container-fluid">
+        <br />
+        {tour.map((m) => (
+          <TourCard key={m.id} m={m} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Tour;
